1f7fd5672899b1db8bf17f0d6a67d7a1d1a2842f
ahmeterdogann <ahmett.erdogan.23@gmail.com>
2023-07-10 22:49:46 +0300

    bug fix

--- src/main/java/com/ahmeterdogan/util/DatabaseUtil.java
+++ src/main/java/com/ahmeterdogan/util/DatabaseUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseUtil {
     private final static String URL = "jdbc:postgresql://localhost/moviedb";
     private final static String USER = "postgres";
-    private final static String PASSWORD = "ahmet1903";
+    private final static String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
